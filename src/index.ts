2983232b122b586519617b8d6dd1cf72b497fb7d
Jeffrey Castellano <jcastellano@gmail.com>
2025-06-06 00:23:18 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // index.ts
 export { useStatsPanel } from './hooks/useStatsPanel';
 export { stats, statsPlugin } from './plugin';
-export { useComputeTracker } from './utils/webgpu-compute-tracker';
+export { useComputeTracker,trackComputeTime } from './utils/webgpu-compute-tracker';
 export type { StatsOptions, StatsData } from './types';
