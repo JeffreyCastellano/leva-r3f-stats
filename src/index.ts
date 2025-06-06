06c704446538602d3d3b20fd4ddc04cf798d4e6b
Jeffrey Castellano <jcastellano@gmail.com>
2025-06-06 00:17:24 -0400

    Expose options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // index.ts
 export { useStatsPanel } from './hooks/useStatsPanel';
 export { stats, statsPlugin } from './plugin';
+export { useComputeTracker } from './utils/webgpu-compute-tracker';
 export type { StatsOptions, StatsData } from './types';
